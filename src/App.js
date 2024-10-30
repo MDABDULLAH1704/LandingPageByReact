ec4c5f828e566845d688c330277370b2be69292b
MDABDULLAH1704 <143082157+MDABDULLAH1704@users.noreply.github.com>
2024-10-30 13:16:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ import Home from "./pages/Home";
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <Home />
-=======
-      hello world ! How are you.
->>>>>>> 0c42bb39c6275e4c29419d4c77d151ab2d4819d7
     </>
   );
 }
