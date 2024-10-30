da40dba734916ae0ca5d619f54d90bc980d76009
MDABDULLAH1704 <mdabdullahqais123@gmail.com>
2024-10-30 19:45:08 +0530

    merge2 change

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
         <section className='section-hero' id='section-hero'>
             <div className='hero'>
                 <div className="heroLeft">
-                    <h1>Transform You into a Warrior!</h1>
+                    <h1>Transform You into a <span>Warrior!</span></h1>
                     <p>A fitness gym is a dedicated space for physical training, offering equipment and classes to improve strength, endurance, and overall health. It fosters community, motivation, and personal growth.</p>
                 </div>
 
