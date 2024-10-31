e8d7388019069632e5d9a6a07281c296a809bc46
MDABDULLAH1704 <mdabdullahqais123@gmail.com>
2024-10-31 13:50:26 +0530

    changes40

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import './Gallery.css'
+import gallery1 from '../../assets/gallery1.jpg'
+import gallery2 from '../../assets/gallery2.jpg'
+import gallery3 from '../../assets/gallery3.jpg'
+import gallery4 from '../../assets/gallery4.jpeg'
+import gallery5 from '../../assets/gallery5.jpg'
+import gallery6 from '../../assets/gallery6.jpg'
 
 const Gallery = () => {
     return (
         <section className='section-gallery' id='section-gallery'>
-            <div>
-                Gallery
+            <div className='gallery'>
+                <h2>Our Gallery</h2>
+
+                <div className="gallery-div">
+                    <div><img src={gallery1} alt="Image" /></div>
+                    <div><img src={gallery2} alt="Image" /></div>
+                    <div><img src={gallery3} alt="Image" /></div>
+                    <div><img src={gallery4} alt="Image" /></div>
+                    <div><img src={gallery5} alt="Image" /></div>
+                    <div><img src={gallery6} alt="Image" /></div>
+                </div>
             </div>
         </section>
     )
