78842d33ea65db6f1224b9f9b5eada41d0ceb0a7
MDABDULLAH1704 <mdabdullahqais123@gmail.com>
2024-10-31 19:16:05 +0530

    Your commit message here

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,23 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Contact.css'
 import { FaMapMarkerAlt } from 'react-icons/fa';
 import { FaPhone } from 'react-icons/fa';
 import { FaEnvelope } from 'react-icons/fa';
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 
 const Contact = () => {
+    // For AOS Animation
+    useEffect(() => {
+        AOS.init({
+            duration: 800,
+        });
+    }, []);
+
     return (
-        <section className='section-contact' id='section-contact'>
-            <div className='contact'>
+        <>
+            <div className='contact' id='section-contact'>
                 <h2> Contact Us </h2>
 
-                <div className="contact-div">
-                    <p><span><FaMapMarkerAlt /></span> 321 Steel City Way, Lafayette, CA 94549</p>
+                <div className="contact-div" data-aos="zoom-in">
+                    <p><span><FaMapMarkerAlt /></span> 321 Steel City Way, Lafayette</p>
                     <p><span><FaPhone /></span> (925) 555-0011</p>
                     <p><span><FaEnvelope /></span>  inquiries@fitnessgym.com</p>
                 </div>
             </div>
-        </section>
+
+            <hr />
+        </>
     )
 }
 
