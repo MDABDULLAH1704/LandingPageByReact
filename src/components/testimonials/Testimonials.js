9141eae6e546954f725c7acede29f25a1b401b06
MDABDULLAH1704 <143082157+MDABDULLAH1704@users.noreply.github.com>
2024-11-02 21:04:25 +0530

    Update Testimonials.js

--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -1,5 +1,8 @@
 import React, { useEffect } from 'react'
 import './Testimonials.css'
+import testimonials1 from '../../assets/testimonials1.jpg'
+import testimonials2 from '../../assets/testimonials2.jpg'
+import testimonials3 from '../../assets/testimonials3.jpg'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
@@ -18,17 +21,17 @@ const Testimonials = () => {
             <div className="testimonials-div" data-aos="zoom-in">
                 <div data-aos="fade" data-aos-delay="300">
                     <p>"Joining Fitness GYM was one of the best decisions I've ever made! The trainers are incredibly supportive and have tailored a program just for me. I love the variety of classes offered, and I've never felt more motivated. This place truly feels like a community and everyone lifts each other up!"</p>
-                    <h3>Riya S</h3>
+                    <h3>Riya <img src={testimonials1} /></h3>
                 </div>
 
                 <div data-aos="fade" data-aos-delay="300">
                     <p>"As someone  who was new to fitness, I was nervous about starting my journey. But the team at Fitness GYM made it easy and enjoyable. The environment is welcoming, and I appreciate how the trainers focus on individual needs. I've seen amazing progress in just a few months!"</p>
-                    <h3>Raj K</h3>
+                    <h3>David <img src={testimonials2} /></h3>
                 </div>
 
                 <div data-aos="fade" data-aos-delay="300">
                     <p>"Fitness GYM has changed my life! The state-of-the-art equipment and diverse classes keep my workouts exciting. The trainers are knowledgeable and genuinely care about my progress. I'm not just reaching my fitness goals; I'm having fun while doing it!"</p>
-                    <h3>Meera T</h3>
+                    <h3>Tara <img src={testimonials3} /></h3>
                 </div>
             </div>
         </div>
